a4340cf57b6efdad50e1e5154898436f3f6babf4
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2024-11-26 11:27:15 +0300

    Initial Commit

---
+++ CoffeeShopApp/CoffeeShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopAppApp.swift
+//  CoffeeShopApp
+//
+//  Created by Ahmet Bostancıklıoğlu on 26.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
